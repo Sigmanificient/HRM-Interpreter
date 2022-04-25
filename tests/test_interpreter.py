f9aa3fdfc0551ed3ebb505f089e2e0001a6ccd4e
sigmanificient <edhyjox@gmail.com>
2022-04-25 23:24:35 +0200

    :white_check_mark: Adding test for comment & outbox

---
+++ tests/test_interpreter.py
@@ -0,0 +1,20 @@
+import io
+from contextlib import redirect_stdout
+from src.hrm.first_iteration import Interpreter
+
+
+def test_comments():
+    interpreter = Interpreter(['# a comment', '#'])
+    assert len(interpreter.program) == 0
+
+
+def test_outbox():
+    program = Interpreter(['outbox 1', 'outbox 2'])
+
+    f = io.StringIO()
+    with redirect_stdout(f):
+        for line in program:
+            program.process(line)
+
+    out = f.getvalue()
+    assert out == '-> 1\n-> 2\n'
