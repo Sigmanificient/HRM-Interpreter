423e02b3f0ec478d25d159c6e873b4b2c47410e9
sigmanificient <edhyjox@gmail.com>
2022-08-13 03:38:10 +0200

    :recycle: Used hrm instructions to shorten example code

--- examples/truth_sim_hrm.py
+++ examples/truth_sim_hrm.py
@@ -1,58 +1,10 @@
-__state = {
-    'inbox': iter([1]),
-    'outbox': [],
-    'buffer': {},
-    'line': 0,
-    'hold': None
-}
-
-
-def inbox(state):
-    state['hold'] = next(state['inbox'])
-
-
-def outbox(state):
-    out = state.get('hold')
-    if out is None:
-        raise ValueError
-
-    state['hold'] = None
-    state['outbox'].append(out)
-    print(state['outbox'][-1])
-
-
-def jump_z(line):
-    def __jump_z(state):
-
-        if state['hold'] == 0:
-            state['line'] = line - 2
-
-    return __jump_z
-
-
-def jump(line):
-    def __jump(state):
-        state['line'] = line - 2
-
-    return __jump
-
-
-def copy_to(buffer_id):
-    def __copy_to(state):
-        state['buffer'][buffer_id] = state['hold']
-    return __copy_to
-
-
-def copy_from(buffer_id):
-    def __copy_from(state):
-        state['hold'] = state['buffer'][buffer_id]
-
-    return __copy_from
+from hrm.instructions import inbox, jump_z, copy_to, outbox, copy_from, jump
+from hrm.state import State
 
 
 instructions = [
     inbox,
-    jump_z(7),
+    jump_z(6),
     copy_to(0),
     outbox,
     copy_from(0),
@@ -61,10 +13,11 @@ instructions = [
 ]
 
 
-EOF = len(instructions)
+__state = State([0], eof=len(instructions))
 
-while __state['line'] != EOF:
-    ins = instructions[__state['line']]
+
+while __state.line != __state.eof:
+    ins = instructions[__state.line]
 
     ins(__state)
-    __state['line'] += 1
+    __state.line += 1
